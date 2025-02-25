c94a0dd77b2d037dcea32f43b58c58e98a0f102a
spaghetti83 <carbonetti.marco@gmail.com>
2025-02-25 12:15:08 +0100

    web components funzionante (theme-selector)

--- index.js
+++ index.js
@@ -1,11 +1,56 @@
 
-const loadScript = () => {
 
-    const toggleMain = document.getElementById('theme-selector')
-    const circle = document.getElementById('circle')
-    const sky = document.getElementById('tgl-box')
-    const stars = document.getElementsByClassName('star')
-    const cloud = document.getElementsByClassName('cloud')
+
+class ThemeSelector extends HTMLElement{
+    constructor(){
+        super()
+
+        const shadow = this.attachShadow({mode: 'open'})
+
+        fetch('./theme-toggle/theme-toggle.html')
+        .then(response => response.text())
+        .then(html => {
+            const link = document.createElement('link')
+            link.rel = 'stylesheet'
+            link.href = './theme-toggle/toggle-style.css'
+            const script = document.createElement('script')
+            script.src = './theme-toggle/transition.js'
+            const htmlPage = document.createElement('div')
+            htmlPage.innerHTML = html
+            
+            const toggleEl = htmlPage.querySelector('#main-toggle')
+            console.log(toggleEl)
+            shadow.appendChild(link)
+            link.addEventListener('load',()=>{ 
+                shadow.appendChild(toggleEl)
+                shadow.appendChild(script);
+                script.addEventListener('load',()=>{
+                    exportedScript()
+            })
+        })
+            
+        })
+        .catch( err => console.log(err))
+        
+        
+
+
+    }
+}
+customElements.define('theme-selector',ThemeSelector)
+
+
+
+const loadScript = () => {
+    const themeElement = document.querySelector('theme-selector')
+    
+    const shadow = themeElement.shadowRoot
+    
+    const toggleMain = shadow.querySelector('theme-selector')
+    const circle = shadow.querySelector('circle')
+    const sky = shadow.querySelector('tgl-box')
+    const stars = shadow.querySelector('star')
+    const cloud = shadow.querySelector('cloud')
     circle.style.transition = '0.4s ease-in-out'
     sky.style.transition = '0.4s ease-in-out'
     toggleMain.addEventListener('touchstart' && 'click', () => {
@@ -13,8 +58,6 @@ const loadScript = () => {
         if (circle.classList.contains('sun')) {
             circle.classList.remove('sun')
             circle.style.top = '150%'
-            /*  circle.style.boxShadow = 'inset 0px -20px 20px 15px #EA3FB8'
-             sky.style.boxShadow = 'inset 0px -20px 20px 15px #EA3FB8' */
             setTimeout(() => {
                 circle.classList.add('moon')
                 circle.style.backgroundColor = '#FFFCE6'
@@ -47,44 +90,6 @@ const loadScript = () => {
     })
 
 }
-
-
-
-
-
-class ThemeSelector extends HTMLElement{
-    constructor(){
-        super()
-
-        const shadow = this.attachShadow({mode: 'open'})
-
-        fetch('./theme-toggle/theme-toggle.html')
-        .then(response => response.text())
-        .then(html => {
-            const link = document.createElement('link')
-            link.rel = 'stylesheet'
-            link.href = './theme-toggle/toggle-style.css'
-            const script = document.createElement('script')
-            script.src = './theme-toggle/transition.js'
-            const htmlPage = document.createElement('div')
-            htmlPage.innerHTML = html
-            
-            const toggleEl = htmlPage.querySelector('#main-toggle')
-            console.log(toggleEl)
-            shadow.appendChild(link)
-            link.addEventListener('load',()=> shadow.appendChild(toggleEl))
-            toggleEl.addEventListener('load',()=> loadScript())
-
-            
-        })
-        .catch( err => console.log(err))
-        
-        
-
-
-    }
-}
-customElements.define('theme-selector',ThemeSelector)
 
 
 
--- theme-toggle/transition.js
+++ theme-toggle/transition.js
@@ -1,18 +1,20 @@
 
+//THIS SCRIPT IS THE ORIGINAL SCRIPT FOR THE THEME TOGGLE
+/*
 const toggleMain = document.getElementById('main-toggle')
 const circle = document.getElementById('circle')
 const sky = document.getElementById('tgl-box')
 const stars = document.getElementsByClassName('star')
 const cloud = document.getElementsByClassName('cloud')
-/* circle.style.transition = '0.4s ease-in-out'
-sky.style.transition = '0.4s ease-in-out' */
+circle.style.transition = '0.4s ease-in-out'
+sky.style.transition = '0.4s ease-in-out'
 toggleMain.addEventListener('touchstart' && 'click', () => {
     
     if (circle.classList.contains('sun')) {
         circle.classList.remove('sun')
         circle.style.top = '150%'
-       /*  circle.style.boxShadow = 'inset 0px -20px 20px 15px #EA3FB8'
-        sky.style.boxShadow = 'inset 0px -20px 20px 15px #EA3FB8' */
+        //circle.style.boxShadow = 'inset 0px -20px 20px 15px #EA3FB8'
+       // sky.style.boxShadow = 'inset 0px -20px 20px 15px #EA3FB8'
         setTimeout(() => {
             circle.classList.add('moon')
             circle.style.backgroundColor = '#FFFCE6'
@@ -42,4 +44,58 @@ toggleMain.addEventListener('touchstart' && 'click', () => {
                }
         }, 400)
     }
-})
+}) */
+
+
+//THIS FUNCTI0N IS EXPORTED AND LOADED TO INDEX.JS (MAIN SPAGHETTI CODE SITE)
+const exportedScript = () => {
+
+    const themeElement = document.querySelector('theme-selector')
+    const shadow = themeElement.shadowRoot
+    const toggleMain = shadow.getElementById('main-toggle')
+    
+    const circle = shadow.getElementById('circle')
+    const sky = shadow.getElementById('tgl-box')
+    const stars = shadow.querySelectorAll('.star')
+    const cloud = shadow.querySelectorAll('cloud')
+    circle.style.transition = '0.4s ease-in-out'
+    sky.style.transition = '0.4s ease-in-out'
+    toggleMain.addEventListener('touchstart' && 'click', () => {
+    console.log('STARS',stars)
+        if (circle.classList.contains('sun')) {
+            circle.classList.remove('sun')
+            circle.style.top = '150%'
+            //circle.style.boxShadow = 'inset 0px -20px 20px 15px #EA3FB8'
+           // sky.style.boxShadow = 'inset 0px -20px 20px 15px #EA3FB8'
+            setTimeout(() => {
+                circle.classList.add('moon')
+                circle.style.backgroundColor = '#FFFCE6'
+                circle.style.boxShadow = 'inset 0px -20px 20px 15px #D3C59B'
+                circle.style.top = '50%'
+                sky.style.boxShadow = 'inset 0px -20px 20px  rgba(1, 57, 153,0.6)'
+                sky.style.backgroundColor = '#002971'
+               for (let i = 0; i < stars.length; i++) {
+                stars[i].style.transition = '0.8s ease-in-out'
+                stars[i].style.backgroundColor = 'white'
+                console.log(stars[i])
+               }
+            }, 400)
+        }else{
+            circle.style.top = '150%'
+            setTimeout(() => {
+                circle.classList.add('sun')
+                circle.style.top = '50%'
+                circle.style.backgroundColor = '#FFDF00'
+                circle.style.boxShadow = 'inset 20px 0px 15px 15px #FEC108'
+                circle.style.top = '50%'
+                sky.style.boxShadow = 'inset 0px 20px 20px 15px  rgba(1, 57, 153,0.6)'
+                sky.style.backgroundColor = '#74A7FF'
+                for (let i = 0; i < stars.length; i++) {
+                    stars[i].style.backgroundColor = 'transparent'
+                    console.log(stars[i])
+                   }
+            }, 400)
+        }
+    })
+
+}
